000374daa57f487382279f1e8f231afa850d4758
jbaans <42092978+jbaans@users.noreply.github.com>
2021-12-16 00:27:18 +0100

    Fix permissions

--- deployIOThermostat.sh
+++ deployIOThermostat.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-mkdir -p /srv/http/iothermostat
+if [ $EUID -e 0 ]; then
+    echo "Please run script as regular user, not as root. Aborting."
+    exit 2
+fi
+
+sudo mkdir -p /srv/http/iothermostat
 sudo cp -R iothermostat-http/* /srv/http/iothermostat
 cp -R iothermostat-python ~
